d1b1ad9b13bc3493e6942eee6a3b5240b4212836
DESKTOP-PAUQ0TK\Admin <trongdai306@gmail.com>
2023-09-24 18:41:02 +0700

    first commit

---
+++ src/main/java/com/controller/WelcomeRestController.java
@@ -0,0 +1,11 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+
+}
---
+++ src/main/java/com/daitt/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.daitt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/welcome/{name}")
+	public String welcome(@PathVariable("name") String name) {
+		return name+", Welcome to DaiTT Family..!!";
+	}
+
+}
